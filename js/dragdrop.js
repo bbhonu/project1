2973654671e8289ca7069df9202122b9139ac8a5
Hannah Vine <hannahvine@Hannahs-MacBook-Pro-2.local>
2017-03-20 20:28:51 -0600

    took out unnecessary stuff

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,17 +1,4 @@
-function drag(e) {
-	e.dataTransfer.setData("Text", this.id);
-}
 
-function drop(e) {
-	var words;
-	words=e.dataTransfer.getData("Text");
-	this.appendChild(document.getElementById(words));
-	e.preventDefault();
-}
-
-function cancelEvent(e){ 
-	e.preventDefault();
-}
 
 var drophere = document.getElementById("drophere");
 drophere.addEventListener("drop", drop);
